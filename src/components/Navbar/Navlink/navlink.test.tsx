afa3c06f5703c5f59b3b03306c59e0f392e40ebf
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-06-10 04:10:17 +0000

    test: add navlink should be clickable

--- src/components/Navbar/Navlink/navlink.test.tsx
+++ src/components/Navbar/Navlink/navlink.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 import Navlink from ".";
 
@@ -12,5 +13,17 @@ describe("Navlink test", () => {
     expect(screen.getByText("Beranda")).toBeInTheDocument();
     expect(screen.getByText("Kategori")).toBeInTheDocument();
     expect(screen.getByText("Tentang Kami")).toBeInTheDocument();
+  });
+
+  test("Links should be clickable", () => {
+    render(
+      <BrowserRouter>
+        <Navlink />
+      </BrowserRouter>
+    );
+    const categoryLink = screen.getByText("Kategori");
+    expect(categoryLink.getAttribute("href")).toBe("/category");
+    userEvent.click(categoryLink);
+    expect(global.window.location.href).toContain("/category");
   });
 });
