bbf9aafa22afccb682f4cf74990d3ded9fdd73f7
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-06-11 10:56:49 +0000

    feat: add category section in home page

---
+++ src/components/pages/home/CategorySection/index.tsx
@@ -0,0 +1,30 @@
+import {
+  Center,
+  Container,
+  Grid,
+  GridItem,
+  Heading,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+
+const CategorySection = () => {
+  return (
+    <Container maxW="6xl" py="16">
+      <VStack as={Center}>
+        <Heading>Kategori</Heading>
+        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
+      </VStack>
+      <Grid templateColumns="repeat(5, 1fr)" mt="16">
+        <GridItem>
+          <Text>Hello</Text>
+        </GridItem>
+        <GridItem>
+          <Text>Hello</Text>
+        </GridItem>
+      </Grid>
+    </Container>
+  );
+};
+
+export default CategorySection;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@chakra-ui/react";
+import CategorySection from "components/pages/home/CategorySection";
 import HeroSection from "components/pages/home/HeroSection";
 
 const Home = () => {
   return (
     <Box minH="100vh">
       <HeroSection />
+      <CategorySection />
     </Box>
   );
 };
