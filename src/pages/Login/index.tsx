0447828ec920ae768445cd32b573abb42f60855c
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-06-13 10:11:38 +0000

    feat: add basic login page

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
+const Login = () => {
+  return (
+    <>
+      <Button>Login</Button>
+    </>
+  );
+};
+
+export default Login;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import Home from "pages/Home";
+import Login from "pages/Login";
 import { Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
