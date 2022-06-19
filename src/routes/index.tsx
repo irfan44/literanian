42593d85ab8dfab8ab5dc25d8f5aa972096dc30c
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-06-19 06:25:53 +0000

    fix: change login modal to login page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import Explore from "pages/Explore";
 import Home from "pages/Home";
+import Login from "pages/Login";
 import { Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/explore" element={<Explore />} />
     </Routes>
   );
