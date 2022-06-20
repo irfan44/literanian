64d1e2dba4f0ba69162db749c1033aa0d42b3efb
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-06-20 23:03:04 +0000

    fix: add styling to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,34 @@
-import { Center, Container, Heading, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import Navlink from "components/Navbar/Navlink";
 
 const Footer = () => {
   return (
-    <Container maxW="6xl" py="8">
-      <VStack as={Center}>
-        <Heading>Logo</Heading>
-        <Text>Dibuat dengan ❤️ oleh tim FE-ED6</Text>
-        <Text>Copyright © 2022 (nama brand). All right reserved</Text>
-      </VStack>
-    </Container>
+    <Box bg="black" color="white">
+      <Container maxW="6xl" py="16">
+        <SimpleGrid
+          templateColumns={{ sm: "1fr 1fr", md: "2fr 1fr" }}
+          spacing={8}
+        >
+          <Box>
+            <Heading mb="10">Logo</Heading>
+            <Text>Copyright © 2022 (nama brand). All right reserved</Text>
+            <Text>Dibuat oleh tim FE-ED6</Text>
+          </Box>
+          <Stack align={"flex-start"}>
+            <Navlink />
+          </Stack>
+        </SimpleGrid>
+        <VStack></VStack>
+      </Container>
+    </Box>
   );
 };
 
