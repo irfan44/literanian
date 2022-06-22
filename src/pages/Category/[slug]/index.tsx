bbff37edb91b988f665ebe3ec22d278f89d017cf
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-06-22 11:17:41 +0000

    fix: add styling to article by category list

--- src/pages/Category/[slug]/index.tsx
+++ src/pages/Category/[slug]/index.tsx
@@ -1,3 +1,12 @@
+import {
+  Box,
+  Center,
+  Container,
+  Heading,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { fetchArticlesByCategory } from "api/graphcms";
 import ArticleCard from "components/ArticleCard";
 import { useEffect, useState } from "react";
@@ -32,11 +41,21 @@ const CategoryArticles = () => {
   }, []);
 
   return (
-    <>
-      {articles.map((article) => {
-        return <ArticleCard key={article.slug} {...article} />;
-      })}
-    </>
+    <Box minH="100vh" bg="#f6f8fd" pb="16">
+      <Container maxW="6xl">
+        <VStack as={Center} py="16">
+          <Text fontSize="sm" color="blackAlpha.700">
+            Kategori
+          </Text>
+          <Heading textTransform="capitalize">{slug}</Heading>
+        </VStack>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing="6">
+          {articles.map((article) => {
+            return <ArticleCard key={article.slug} {...article} />;
+          })}
+        </SimpleGrid>
+      </Container>
+    </Box>
   );
 };
 
