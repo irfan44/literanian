ba7bbf94b9a64da64f26cdb6d3818d4aa78da830
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-06-23 08:03:30 +0000

    fix: add prose to rich text

--- src/pages/ArticleContent/index.tsx
+++ src/pages/ArticleContent/index.tsx
@@ -1,9 +1,10 @@
-import { Heading } from "@chakra-ui/react";
+import { Box, Container, Heading } from "@chakra-ui/react";
 import { fetchArticleBySlug } from "api/graphcms";
 import { useParams } from "react-router";
 import { useEffect, useState } from "react";
-import { ArticleContent} from "types/article";
-import ReactMarkdown from "react-markdown";
+import { ArticleContent } from "types/article";
+import { RichText } from "@graphcms/rich-text-react-renderer";
+import { Prose } from "@nikolovlazar/chakra-ui-prose";
 
 const ContentArticle = () => {
   const [article, setArticle] = useState<ArticleContent>();
@@ -22,21 +23,21 @@ const ContentArticle = () => {
 
   useEffect(() => {
     getArticleContent();
-    console.log(slug);
   }, []);
 
-  const markdown = article &&
-  article.content.markdown.replace(/\n/g, `<br>`);
-
   return (
-    <>
-      {article && markdown && (
-        <>
-          <Heading>{article.title}</Heading>
-          <ReactMarkdown>{markdown}</ReactMarkdown>
-        </>
-      )}
-    </>
+    <Box minH="100vh" bg="#f6f8fd" py="16">
+      <Container maxW="4xl">
+        {article && (
+          <>
+            <Heading mb="4">{article.title}</Heading>
+            <Prose>
+              <RichText content={article.content.raw} />
+            </Prose>
+          </>
+        )}
+      </Container>
+    </Box>
   );
 };
 
