5a03d1432c2ffc982cd1aaa4d53774771cc7bea3
Dyah Ayu Arimbi S.K.W.P <101874377+dyahayuarimbi@users.noreply.github.com>
2022-06-23 11:45:25 +0700

    Konten Artikel

--- src/pages/ArticleContent/index.tsx
+++ src/pages/ArticleContent/index.tsx
@@ -1,9 +1,43 @@
-const ArticleContent = () => {
+import { Heading } from "@chakra-ui/react";
+import { fetchArticleBySlug } from "api/graphcms";
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { ArticleContent} from "types/article";
+import ReactMarkdown from "react-markdown";
+
+const ContentArticle = () => {
+  const [article, setArticle] = useState<ArticleContent>();
+  const { slug } = useParams();
+
+  const getArticleContent = async () => {
+    if (slug) {
+      try {
+        const article = await fetchArticleBySlug(slug);
+        setArticle(article);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getArticleContent();
+    console.log(slug);
+  }, []);
+
+  const markdown = article &&
+  article.content.markdown.replace(/\n/g, `<br>`);
+
   return (
     <>
-      <h1>Artikel</h1>
+      {article && markdown && (
+        <>
+          <Heading>{article.title}</Heading>
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </>
+      )}
     </>
   );
 };
 
-export default ArticleContent;
+export default ContentArticle;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import Explore from "pages/Explore";
 import Home from "pages/Home";
 import Login from "pages/Login";
 import { Route, Routes } from "react-router-dom";
+import ArticleContent from "pages/ArticleContent";
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ const AppRouter = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/explore" element={<Explore />} />
+      <Route path="/article/:slug"element={<ArticleContent />} />
     </Routes>
   );
 };
