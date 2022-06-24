3834a7af6d2f2fbb6e010155f3ee317010aa94b4
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2022-06-24 01:41:41 +0000

    feat: add premium article check & badge

--- src/components/ArticleCard/index.tsx
+++ src/components/ArticleCard/index.tsx
@@ -20,11 +20,13 @@ const ArticleCard = ({
   category,
   createdAt,
   excerpt,
+  articleType,
 }: ArticleData) => {
   const { url } = coverImage;
   const { uid } = useAppSelector((state) => state.userProfile);
 
   const date = formatDate(createdAt);
+  const articlePremium = articleType === "premium";
 
   return (
     <VStack
@@ -55,6 +57,12 @@ const ArticleCard = ({
         <Badge bg="#D6E6F5">{category}</Badge>
         <Text fontSize="sm">|</Text>
         <Text fontSize="sm">{date}</Text>
+        <Text fontSize="sm">|</Text>
+        {articlePremium && (
+          <Badge mb="2" variant="solid" colorScheme="yellow">
+            Premium
+          </Badge>
+        )}
       </HStack>
       <Text fontWeight={400} color={"gray.600"} fontSize={"16px"} noOfLines={3}>
         {excerpt}
